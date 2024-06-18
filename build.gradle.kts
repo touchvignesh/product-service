4ec2f8f737517074a0206e9c25a000d9ab4851ae
Vignesh Sabapathi <touchvignesh@gmail.com>
2024-06-18 14:30:41 +0530

    commit to raise new PR
    commit to raise new PR

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,7 @@ tasks.named<Jar>("jar") {
 spotless {
     java {
         target("**/*.java")
+        // Commit for new pull request
         targetExclude("${layout.buildDirectory}/**/*.java")
         replaceRegex("Remove wildcard import statements", "import\\s+(?:static\\s+)?[^\\*\\s]+\\*;(\\r\\n|\\r|\\n)", "$1")
         toggleOffOn()
